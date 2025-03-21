a8fbd27497b36a3c85532fe4d4291b9fd6500f54
Maria <mariatc025@gmail.com>
2025-03-22 00:35:09 +0100

    changed database name and password

--- php/db.php
+++ php/db.php
@@ -1,8 +1,8 @@
 <?php
 $host = "localhost"; // Change if necessary
-$dbname = "mydb";
-$username = "root"; // Change if using a different user
-$password = ""; // Set MySQL password
+$dbname = "drugsearch";
+$username = "drugsearch"; // Change if using a different user
+$password = "dbw_drugsearch"; // Set MySQL password
 
 try {
     $pdo = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8", $username, $password);
