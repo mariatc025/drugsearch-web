a89ed16f155ff97cbb8ad866052d52ab5f4a9d70
Maria <mariatc025@gmail.com>
2026-03-29 22:10:03 +0200

    add environment variable configuration

--- php/db.php
+++ php/db.php
@@ -1,13 +1,15 @@
 <?php
-$host = "localhost"; // Change if necessary
-$dbname = "drugsearch";
-$username = "drugsearch"; // Change if using a different user
-$password = "dbw_drugsearch"; // Set MySQL password
+$dotenv = parse_ini_file(__DIR__ . '/../.env');
+
+$host     = $dotenv['DB_HOST'];
+$dbname   = $dotenv['DB_NAME'];
+$username = $dotenv['DB_USER'];
+$password = $dotenv['DB_PASS'];
 
 try {
     $pdo = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8", $username, $password);
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch (PDOException $e) {
-    die("Connection failed: " . $e->getMessage());
+    die("Connection failed.");
 }
 ?>
